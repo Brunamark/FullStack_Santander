9230b580bf142d386ed764cd29d3e833b18a5a37
Brunamark <bruninha.markowisk@hotmail.com>
2023-08-30 21:12:21 -0300

    Adicionado Projetos Satander Java

---
+++ reuniaoAcionistas/reuniaoAcionistas/src/Main.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) throws ParseException {
+    // Lê os dados de Entrada conforme descrito neste Desafio.
+    Scanner scanner = new Scanner(System.in);
+    String dataInicial = scanner.nextLine();
+    String dataFinal = scanner.nextLine();
+
+    SistemaAcionistas sistemaAcionistas = new SistemaAcionistas();
+    List < String > analises = sistemaAcionistas.obterAnalisesDesempenho(dataInicial, dataFinal);
+
+    for (String analise: analises) {
+      System.out.println(analise);
+    }
+  }
+}
+
+class SistemaAcionistas {
+  public List < String > obterAnalisesDesempenho(String dataInicialStr, String dataFinalStr) throws ParseException {
+    SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+    Date dataInicial = df.parse(dataInicialStr);
+    Date dataFinal = df.parse(dataFinalStr);
+
+    // Simula uma base da dados em uma lista de análises:
+    List < Analise > analises = new ArrayList < > ();
+    analises.add(new Analise(df.parse("01/01/2023"), "Analise de Desempenho Financeiro"));
+    analises.add(new Analise(df.parse("15/02/2023"), "Analise de Riscos e Exposicoes"));
+    analises.add(new Analise(df.parse("31/03/2023"), "Analises Corporativas"));
+    analises.add(new Analise(df.parse("01/04/2023"), "Analise de Politicas e Regulamentacoes"));
+    analises.add(new Analise(df.parse("15/05/2023"), "Analise de Ativos"));
+    analises.add(new Analise(df.parse("30/06/2023"), "Analise de Inovacao e Tecnologia"));
+
+   //TODO: Implemente o filtro das análises dentro do período especificado. Dica: Crie uma lista para armazenar as análises filtradas e use um loop for para filtrar as análises.
+     List<String> analisesFiltradas = new ArrayList<>();
+
+    for (Analise analise : analises) {
+      if (analise.data.compareTo(dataInicial) >= 0 && analise.data.compareTo(dataFinal) <= 0) {
+        analisesFiltradas.add(analise.descricao);
+      }/* analise.data.compareTo(dataInicial) >= 0: Compara a data da análise com a data inicial. Se a data da análise for maior ou igual à data inicial, isso significa que a análise ocorre após ou exatamente na data inicial.
+       analise.data.compareTo(dataFinal) <= 0: Compara a data da análise com a data final. Se a data da análise for menor ou igual à data final, isso significa que a análise ocorre antes ou exatamente na data final.*/
+    }
+
+    return analisesFiltradas;
+   
+    
+   // TODO: Retorne a lista de análises filtradas.
+  }
+
+  class Analise {
+    Date data;
+    String descricao;
+
+    public Analise(Date data, String descricao) {
+      this.data = data;
+      this.descricao = descricao;
+    }
+  }
+}
